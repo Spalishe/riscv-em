f04fc81d62385e504528056006b8e2ae735009c3
Spalishe <maksamoshkin2010@gmail.com>
2026-04-17 09:14:19 +0300

    Added new optimization guides

--- src/devices/mmio.cpp
+++ src/devices/mmio.cpp
@@ -16,100 +16,103 @@ Copyright 2026 Spalishe
 */
 
 #include "../../include/devices/mmio.h"
-#include "../../include/devices/uart.hpp"
 #include "../../include/cpu.hpp"
 #include "../../include/csr.h"
+#include "../../include/devices/uart.hpp"
 #include "../../include/dram.h"
 #include "../../include/mmu.hpp"
-#include <optional>
 #include <iostream>
+#include <optional>
 
-std::optional<uint64_t> MMIO::load(HART* hart, uint64_t addr, uint64_t size) {
-    uint64_t o = addr;
-    auto phys = mmu_translate(*mmu, hart, addr, AccessType::LOAD);
-    if (!phys.has_value()) return std::nullopt;
-    uint64_t pa = phys.value();
-
-    if((pa % (size/8)) != 0) {
-        hart_trap(*hart,EXC_LOAD_ADDR_MISALIGNED,addr,false);
-        return std::nullopt;
-    }
-
-    // DRAM
-    if (pa >= DRAM_BASE && pa < DRAM_BASE + DRAM_SIZE) {
-        return dram_load(ram,pa,size);
-    }
-
-    // Devices
-    for (auto* dev : devices) {
-        if (pa >= dev->base && pa < dev->base + dev->size) {
-            return dev->read(hart, pa, size);
-        }
-    }
-
-    // Fault
-    hart_trap(*hart,EXC_LOAD_ACCESS_FAULT,addr,false);
+std::optional<uint64_t> MMIO::load(HART *hart, uint64_t addr, uint64_t size) {
+  uint64_t o = addr;
+  auto phys = mmu_translate(*mmu, hart, addr, AccessType::LOAD);
+  if (!phys.has_value())
     return std::nullopt;
+  uint64_t pa = phys.value();
+
+  if ((pa % (size / 8)) != 0) {
+    hart_trap(*hart, EXC_LOAD_ADDR_MISALIGNED, addr, false);
+    return std::nullopt;
+  }
+
+  // DRAM
+  if (pa >= DRAM_BASE && pa < DRAM_BASE + DRAM_SIZE) {
+    return dram_load(ram, pa, size);
+  }
+
+  // Devices
+  for (auto *dev : devices) {
+    if (pa >= dev->base && pa < dev->base + dev->size) {
+      return dev->read(hart, pa, size);
+    }
+  }
+
+  // Fault
+  hart_trap(*hart, EXC_LOAD_ACCESS_FAULT, addr, false);
+  return std::nullopt;
 }
 
-bool MMIO::store(HART* hart, uint64_t addr, uint64_t size, uint64_t value) {
-    auto phys = mmu_translate(*mmu, hart, addr, AccessType::STORE);
-    if (!phys.has_value()) return false;
-    uint64_t pa = phys.value();
-
-    if(pa % (size/8) != 0) {
-        hart_trap(*hart,EXC_STORE_ADDR_MISALIGNED,addr,false);
-        return false;
-    }
-
-    // DRAM
-    if (pa >= DRAM_BASE && pa < DRAM_BASE + DRAM_SIZE) {
-        dram_store(ram,pa,size,value);
-        return true;
-    }
-
-    // Devices
-    for (auto* dev : devices) {
-        if (pa >= dev->base && pa < dev->base + dev->size) {
-            dev->write(hart, pa, size, value);
-            return true;
-        }
-    }
-    
-    // Fault
-    hart_trap(*hart,EXC_STORE_ACCESS_FAULT,addr,false);
+bool MMIO::store(HART *hart, uint64_t addr, uint64_t size, uint64_t value) {
+  auto phys = mmu_translate(*mmu, hart, addr, AccessType::STORE);
+  if (!phys.has_value())
     return false;
+  uint64_t pa = phys.value();
+
+  if (pa % (size / 8) != 0) {
+    hart_trap(*hart, EXC_STORE_ADDR_MISALIGNED, addr, false);
+    return false;
+  }
+
+  // DRAM
+  if (pa >= DRAM_BASE && pa < DRAM_BASE + DRAM_SIZE) {
+    dram_store(ram, pa, size, value);
+    return true;
+  }
+
+  // Devices
+  for (auto *dev : devices) {
+    if (pa >= dev->base && pa < dev->base + dev->size) {
+      dev->write(hart, pa, size, value);
+      return true;
+    }
+  }
+
+  // Fault
+  hart_trap(*hart, EXC_STORE_ACCESS_FAULT, addr, false);
+  return false;
 }
 
-std::optional<uint64_t> MMIO::load_GDB(HART* hart, uint64_t addr, uint64_t size) {
-    // DRAM
-    if (addr >= DRAM_BASE && addr < DRAM_BASE + DRAM_SIZE) {
-        return dram_load(ram,addr,size);
-    }
+std::optional<uint64_t> MMIO::load_GDB(HART *hart, uint64_t addr,
+                                       uint64_t size) {
+  // DRAM
+  if (addr >= DRAM_BASE && addr < DRAM_BASE + DRAM_SIZE) {
+    return dram_load(ram, addr, size);
+  }
 
-    // Devices
-    for (auto* dev : devices) {
-        if (addr >= dev->base && addr < dev->base + dev->size) {
-            return dev->read(hart, addr, size);
-        }
+  // Devices
+  for (auto *dev : devices) {
+    if (addr >= dev->base && addr < dev->base + dev->size) {
+      return dev->read(hart, addr, size);
     }
-    return std::nullopt;
+  }
+  return std::nullopt;
 }
 
-void MMIO::store_GDB(HART* hart, uint64_t addr, uint64_t size, uint64_t value) {
-    // DRAM
-    if (addr >= DRAM_BASE && addr < DRAM_BASE + DRAM_SIZE) {
-        dram_store(ram,addr,size,value);
-        return;
-    }
-
-    // Devices
-    for (auto* dev : devices) {
-        if (addr >= dev->base && addr < dev->base + dev->size) {
-            dev->write(hart, addr, size, value);
-            return;
-        }
-    }
-    
+void MMIO::store_GDB(HART *hart, uint64_t addr, uint64_t size, uint64_t value) {
+  // DRAM
+  if (addr >= DRAM_BASE && addr < DRAM_BASE + DRAM_SIZE) {
+    dram_store(ram, addr, size, value);
     return;
+  }
+
+  // Devices
+  for (auto *dev : devices) {
+    if (addr >= dev->base && addr < dev->base + dev->size) {
+      dev->write(hart, addr, size, value);
+      return;
+    }
+  }
+
+  return;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -15,92 +15,116 @@ Copyright 2026 Spalishe
 
 */
 
-#include <iostream>
 #include "../include/argparser.h"
+#include <iostream>
 #include <string>
 
-#include "../include/main.hpp"
 #include "../include/machine.hpp"
+#include "../include/main.hpp"
 #include "../include/termios.hpp"
 
 /*
-	TODO:
-		- Fix the problem that kernel is stuck in arch_cpu_idle -> handle_exception -> arch_cpu_idle
+        TODO:
+    1. Replace pc_hits with array[128], indexing: idx = (pc >> 2) & (SIZE - 1);
+    2. Remove all instances of copying inst_data, make pointers instead
+    3. Replace for(inst_data dat : block->instrs) with for(const inst_data& dat
+   : block->instrs)
+    4. Remove vector from InstructionBlock with array
+    5. Remove all optionals and replace those with return bool & write val at
+   pointer
+    6. Remove PC from inst_data
+    7. Replace bools in inst_data to 1 bit flags
+    8. Change instr_cache size to smth lower
+    9. Optimize devices search
+    10. Replace switches with to lookup table
 
-		-RV64F
-		-RV64D
+                - Fix the problem that kernel is stuck in arch_cpu_idle ->
+   handle_exception -> arch_cpu_idle
 
-		-RTC GoldFish
-		
-		-MMU Fixes(if required)
-		-RVC    <- this is required to run MMU-Linux
+                -RV64F
+                -RV64D
+
+                -RTC GoldFish
+
+                -MMU Fixes(if required)
+                -RVC    <- this is required to run MMU-Linux
 */
 
-int main(int argc, char* argv[]) {
-	Argparser::Argparser parser(argc,argv);
-	parser.setProgramName("RISC-V EM");
-	parser.addArgument("--bios", "File with Machine Level program (bootloader)",true,false,Argparser::ArgumentType::str);
+int main(int argc, char *argv[]) {
+  Argparser::Argparser parser(argc, argv);
+  parser.setProgramName("RISC-V EM");
+  parser.addArgument("--bios", "File with Machine Level program (bootloader)",
+                     true, false, Argparser::ArgumentType::str);
 
-	parser.addArgument("--kernel", "File with Supervisor Level program",false,false,Argparser::ArgumentType::str);
-	parser.addArgument("--image", "File with Image file that will put on VirtIO-BLK",false,false,Argparser::ArgumentType::str);
+  parser.addArgument("--kernel", "File with Supervisor Level program", false,
+                     false, Argparser::ArgumentType::str);
+  parser.addArgument("--image",
+                     "File with Image file that will put on VirtIO-BLK", false,
+                     false, Argparser::ArgumentType::str);
 
-	parser.addArgument("--dtb", "Use specified FDT instead of auto-generated",false,false,Argparser::ArgumentType::str);
-	parser.addArgument("--dumpdtb", "Dumps auto-generated FDT to file",false,false,Argparser::ArgumentType::str);
-	parser.addArgument("--append", "Append command line arguments", false, false, Argparser::ArgumentType::str);
-	#ifdef USE_GDBSTUB
-		parser.addArgument("--gdb", "Starts GDB Stub on port 1512", false, false, Argparser::ArgumentType::def);
-	#endif
+  parser.addArgument("--dtb", "Use specified FDT instead of auto-generated",
+                     false, false, Argparser::ArgumentType::str);
+  parser.addArgument("--dumpdtb", "Dumps auto-generated FDT to file", false,
+                     false, Argparser::ArgumentType::str);
+  parser.addArgument("--append", "Append command line arguments", false, false,
+                     Argparser::ArgumentType::str);
+#ifdef USE_GDBSTUB
+  parser.addArgument("--gdb", "Starts GDB Stub on port 1512", false, false,
+                     Argparser::ArgumentType::def);
+#endif
 
-	parser.parse();
+  parser.parse();
 
-	bool kernel_has = parser.getDefined(1);
-	bool image_has = parser.getDefined(2);
+  bool kernel_has = parser.getDefined(1);
+  bool image_has = parser.getDefined(2);
 
-	bool dtb_has = parser.getDefined(3);
-	bool dtb_dump_has = parser.getDefined(4);
+  bool dtb_has = parser.getDefined(3);
+  bool dtb_dump_has = parser.getDefined(4);
 
-	std::string cmdline_append;
-	if(parser.getDefined(5)) {
-		cmdline_append = parser.getString(5);
-		std::cout << "Custom cmdargs defined: " << cmdline_append << std::endl;
-	}
+  std::string cmdline_append;
+  if (parser.getDefined(5)) {
+    cmdline_append = parser.getString(5);
+    std::cout << "Custom cmdargs defined: " << cmdline_append << std::endl;
+  }
 
-	Machine VM = Machine();
-	VM.core_count = 1;
-	VM.memsize = DRAM_SIZE;
+  Machine VM = Machine();
+  VM.core_count = 1;
+  VM.memsize = DRAM_SIZE;
 
-	machine_create_memory(VM);
+  machine_create_memory(VM);
 
-	bool file_has = parser.getDefined(0);
-	if(file_has) {
-		std::string file = parser.getString(0);
-		VM.memmap.load_file(DRAM_BASE,file);
-	}
-	
-	if(!dtb_has) machine_create_fdt(VM,cmdline_append);
-	machine_create_devices(VM,(image_has ? parser.getString(2) : ""));
-	machine_load_fdt(VM, (dtb_has ? parser.getString(3) : ""), (dtb_dump_has ? parser.getString(4) : ""));
+  bool file_has = parser.getDefined(0);
+  if (file_has) {
+    std::string file = parser.getString(0);
+    VM.memmap.load_file(DRAM_BASE, file);
+  }
 
-	if (kernel_has) {
-		std::string kernel_path = parser.getString(1);
-		VM.memmap.load_file(DRAM_BASE+0x200000,kernel_path);
-	}
-	machine_create_harts(VM);
+  if (!dtb_has)
+    machine_create_fdt(VM, cmdline_append);
+  machine_create_devices(VM, (image_has ? parser.getString(2) : ""));
+  machine_load_fdt(VM, (dtb_has ? parser.getString(3) : ""),
+                   (dtb_dump_has ? parser.getString(4) : ""));
 
-	termios_start();
-	
-	std::thread VM_thread(machine_run,std::ref(VM));
+  if (kernel_has) {
+    std::string kernel_path = parser.getString(1);
+    VM.memmap.load_file(DRAM_BASE + 0x200000, kernel_path);
+  }
+  machine_create_harts(VM);
 
-	#ifdef USE_GDBSTUB
-		bool gdb_stub = parser.getDefined(6);
-		VM.gdb = gdb_stub;
-		if(gdb_stub) {
-			std::thread GDB_thread(GDB_Create,VM.harts[0],&VM);
-			GDB_thread.join();
-		}
-	#endif
+  termios_start();
 
-	VM_thread.join();
-	
-	return 0;
+  std::thread VM_thread(machine_run, std::ref(VM));
+
+#ifdef USE_GDBSTUB
+  bool gdb_stub = parser.getDefined(6);
+  VM.gdb = gdb_stub;
+  if (gdb_stub) {
+    std::thread GDB_thread(GDB_Create, VM.harts[0], &VM);
+    GDB_thread.join();
+  }
+#endif
+
+  VM_thread.join();
+
+  return 0;
 }
