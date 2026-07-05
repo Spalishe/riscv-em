9ed179d6197e36ca356c39aba9bb9ae36e910210
Spalishe <maksamoshkin2010@gmail.com>
2026-07-05 20:13:37 +0300

    Fix Typo

--- include/gui/x11.hpp
+++ include/gui/x11.hpp
@@ -252,7 +252,6 @@ inline void StartEventLoop(AppWindow& appWindow)
 
                     if (appWindow.kb)
                     {
-                    printf("%d %d %d %d %d %d %d %d\n",mods,key1,key2,key3,key4,key5,key6,appWindow.input.pressed_count > 6);
                         appWindow.kb->update(mods, key1, key2, key3, key4, key5, key6,
                                              appWindow.input.pressed_count > 6);
                     }
